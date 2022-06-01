a004f737e257e3f1de5618ecff49f30e3011eb58
suriyabose <suriyakumarbose@gmail.com>
2022-06-01 16:32:12 +0530

    init_commit

---
+++ src/main/java/com/example/amsarticleapi/AmsArticleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.amsarticleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsArticleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsArticleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amsarticleapi/AmsArticleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amsarticleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsArticleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
